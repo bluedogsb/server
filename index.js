afd1b7a03913ab5cd2c9bed9c7e9450ed66b7ebb
natgitinit <natgitinit@gmail.com>
2023-11-27 16:07:56 -0600

    dotenv keys config

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
-const app = express();
+/* GOOGLE AUTH */ 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(
     new GoogleStrategy(
         {
-            clientID: keys.googleClientID,
-            clientSecret: keys.googleClientSecret,
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google/callback'
         },
-        (accessToken, refreshToken, profile, done) => {
+        (accessToken) => {
             console.log(accessToken);
-            done(null, profile);
         }
     )
 );
 
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
+/* PASSPORT SETUP */ 
 
-// app.get('/auth/google/callback',
-//     passport.authenticate('google', { failureRedirect: '/login' }),
-//     function (req, res) {
-//         // Successful authentication, redirect home.
-//         res.redirect('/article');
-//     });
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/error' }),
+    function (req, res) {
+        // Successful authentication, redirect success.
+        res.redirect('/success');
+    });
+
+
+/* EXPRESS PORT */
+const app = express();
+const PORT = process.env.PORT || 3000;
+// app.listen(PORT);
+app.listen(PORT, () => console.log('App listening on port ' + PORT));
