a76914dc5965123c0f59db211e6cd3253b9b486c
natgitinit <natgitinit@gmail.com>
2023-11-27 15:11:17 -0600

    config keys

--- config/keys.js
+++ config/keys.js
@@ -1,15 +1,14 @@
-if (process.env.NODE_ENV === 'production') {
-    //in production - return prod set of keys
-    module.exports = require('./prod');
-} else {
-    module.exports = require('./dev');
-}
-
-
+// if (process.env.NODE_ENV === 'production') {
+//     //in production - return prod set of keys
+//     module.exports = require('./prod');
+// } else {
+//     module.exports = require('./dev');
+// }
 
 module.exports = {
-    googleClientID: '726741164486-osvt9m7n4ur4orla6d5b5undjl5o4cfi.apps.googleusercontent.com',
-    googleClientSecret: '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'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 };
+
 
 
