9ab31676c2554de75e95ed476b13bea04f2ae9e6
natgitinit <natgitinit@gmail.com>
2023-12-01 15:26:39 -0600

    authroutes app

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ require('dotenv').config({ path: 'config.env' });
 require('./models/User');
 const express = require('express');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
+
+/* Mongoose MongoDB */
+const DB = process.env.MONGO_URI;
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(DB);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
 
 /* EXPRESS Server */
 const app = express();
@@ -15,7 +27,11 @@ app.get('/', (req, res) => {
     res.send({ title: 'Users'});
 })
 
-authRoutes(app);
+require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('App listening on port ' + PORT));
+
+
+
+
