260a7361467f5b6464dd2574f88ca809e2687c99
natgitinit <natgitinit@gmail.com>
2023-12-03 13:45:59 -0600

    change port number

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
