f09025cbacecb53d757bcd455ad6f0d327b97ea7
natgitinit <natgitinit@gmail.com>
2023-12-03 14:09:23 -0600

    port 3000

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const connectDB = async () => {
 //     });
 // });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //     console.log(`Listening on port ${PORT}`);
 // });
