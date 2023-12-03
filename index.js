db398de9792cdc748929990bc4a00567991fe4fa
natgitinit <natgitinit@gmail.com>
2023-12-03 14:08:03 -0600

    server request error

--- index.js
+++ index.js
@@ -31,20 +31,20 @@ const connectDB = async () => {
 }
 
 /* ERROR Catch */ 
-app.use((req, res, next) => {
-    const error = new Error('Route not found');
-    error.status = 404;
-    next(error);
-});
+// app.use((req, res, next) => {
+//     const error = new Error('Route not found');
+//     error.status = 404;
+//     next(error);
+// });
 
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message,
-        },
-    });
-});
+// app.use((error, req, res, next) => {
+//     res.status(error.status || 500);
+//     res.json({
+//         error: {
+//             message: error.message,
+//         },
+//     });
+// });
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
