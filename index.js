e79204f7d0d142a0772584f961044216409d5253
natgitinit <natgitinit@gmail.com>
2023-12-03 14:42:05 -0600

    cookie key

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ const connectDB = async () => {
     }
 }
 
+const cookieKey = process.env.COOKIE_KEY
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: process.env.COOKIE_KEY
+        keys: cookieKey
     })
 );
 app.use(passport.initialize());
