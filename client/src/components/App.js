d27c04c335b722262414a870afa3a9767a13a812
natgitinit <natgitinit@gmail.com>
2023-12-22 10:59:46 -0600

    appjs header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+// const Dashboard = () => <h2>Dashboard</h2>;
+// const SurveyNew = () => <h2>SurveyNew</h2>;
 
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
-const App = () => {
-    return (
-        <div>
-            <Header/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" component={Header} />
-                    <Route path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
-};
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <Routes>
+                            <Route path="/" component={Header} />
+                            <Route path="/surveys" component={Dashboard} />
+                            <Route path="/surveys/new" component={SurveyNew} />
+                        </Routes>
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
 
-export default App;
+export default connect(null, actions)(App);
