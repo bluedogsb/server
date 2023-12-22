5eb5309548d121bb2bb27c05b2e300ee72496abc
natgitinit <natgitinit@gmail.com>
2023-12-21 21:03:14 -0600

    adds header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <Routes>
+                    <Route exact={true} path="/" component={<Header/>} />
+                    <Route path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                    <Route path="/" component={<Header/>} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
