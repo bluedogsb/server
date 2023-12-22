7eda53509a2bfefc17f4fa0d5c52e05676d2c103
natgitinit <natgitinit@gmail.com>
2023-12-22 14:47:21 -0600

    actions and reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import 'materialize-css/dist/css/materialize.min.css';
+import "materialize-css/dist/css/materialize.min.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
